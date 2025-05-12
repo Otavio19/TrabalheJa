455a6dbe816b1e4c10fca3bfd6dbf3b31f4fabbf
Otávio Coelho <otavioafonso.elias@gmail.com>
2025-05-11 21:25:46 -0300

    update path

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { getUserLevel } from "../../utils/auth";
+import { getUserLevel } from "../../utils/auth.jsx";
 
 const ProtectedRoute = ({ children, allowedLevels }) => {
   const userLevel = getUserLevel();
